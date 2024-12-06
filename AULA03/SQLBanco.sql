4f524040a7d1a891377009bbeb9a4b2e2b8fff6f
techcomkathia <techcomk@gmail.com>
2024-12-06 09:44:59 -0300

    criação da tabela de ligação livros_categorias

--- AULA03/SQLBanco.sql
+++ AULA03/SQLBanco.sql
@@ -14,4 +14,21 @@ id_categoria INT AUTO_INCREMENT PRIMARY KEY,
 nome VARCHAR(30)
 );
 
+CREATE TABLE livros (
+id_livros INT AUTO_INCREMENT PRIMARY KEY,
+titulo VARCHAR(180) NOT NULL,
+fk_autor INT NOT NULL,
+ano_publicado YEAR,
+resumo TEXT,
+FOREIGN KEY (fk_autor) REFERENCES autores (id_autor)
+);
+
+
+create table livros_categorias(
+fk_livro int not null,
+fk_categoria int not null,
+primary key(fk_livro, fk_categoria),
+foreign key (fk_livro) references livros(id_livros),
+foreign key (fk_categoria) references categorias(id_categoria) 
+)
 
